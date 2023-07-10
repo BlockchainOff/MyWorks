6e94062a9937f164683c4d74cd0768197e795c51
Леонид Захаров <rayli.zaharov@yandex.ru>
2023-07-10 11:26:41 +0300

    Open repo is job

--- seminar_002/obj/Release/net6.0/seminar_002.AssemblyInfo.cs
+++ seminar_002/obj/Release/net6.0/seminar_002.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("seminar_001")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("seminar_002")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("seminar_001")]
-[assembly: System.Reflection.AssemblyTitleAttribute("seminar_001")]
+[assembly: System.Reflection.AssemblyProductAttribute("seminar_002")]
+[assembly: System.Reflection.AssemblyTitleAttribute("seminar_002")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
